0571d7be35c37bf06be17061277fc2ad82a22f56
Vanessa Cunha Rangel Ramos <163733235+vanessacrramos@users.noreply.github.com>
2024-11-14 19:52:47 +0100

    Create exercise_9.sql

---
+++ sql_scripts/exercise_9.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(a.first_name, ' ', a.last_name) AS actor_name, COUNT(*) AS amount_of_starred_movies
+FROM actor a 
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+GROUP BY a.first_name, a.last_name , a.actor_id 
+ORDER BY amount_of_starred_movies DESC
+LIMIT 10
