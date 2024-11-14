1da66d827f82ca667e4150e43d2e77dd01546778
Vanessa Cunha Rangel Ramos <163733235+vanessacrramos@users.noreply.github.com>
2024-11-14 19:51:40 +0100

    Create exercise_8.sql

---
+++ sql_scripts/exercise_8.sql
@@ -0,0 +1,23 @@
+SELECT * FROM (
+SELECT c.city, AVG(f.rental_rate) AS average_rental_rate, 'Highest' AS highest_or_lowest
+FROM city c
+JOIN address a ON c.city_id = a.city_id 
+JOIN store s ON a.address_id = s.address_id
+JOIN inventory i ON i.store_id = s.store_id 
+JOIN film f ON f.film_id = i.film_id 
+GROUP BY c.city_id, c.city
+ORDER BY average_rental_rate DESC
+LIMIT 1
+)
+UNION ALL 
+SELECT * FROM (
+SELECT c.city, AVG(f.rental_rate) AS average_rental_rate, 'Lowest' AS highest_or_lowest
+FROM city c
+JOIN address a ON c.city_id = a.city_id 
+JOIN store s ON a.address_id = s.address_id
+JOIN inventory i ON i.store_id = s.store_id 
+JOIN film f ON f.film_id = i.film_id 
+GROUP BY c.city_id, c.city
+ORDER BY average_rental_rate
+LIMIT 1
+)
