07e827c0b9ffba117f53e55820def73875b975cc
Vanessa Cunha Rangel Ramos <163733235+vanessacrramos@users.noreply.github.com>
2024-11-14 20:54:59 +0100

    Update exercise_10.sql

--- sql_scripts/exercise_10.sql
+++ sql_scripts/exercise_10.sql
@@ -1,3 +1,4 @@
+# List the top 10 actors based on the number of films they've appeared in.
 SELECT CONCAT(a.first_name, ' ', a.last_name) AS actor_name, COUNT(*) AS amount_of_starred_movies
 FROM actor a 
 JOIN film_actor fa ON a.actor_id = fa.actor_id
