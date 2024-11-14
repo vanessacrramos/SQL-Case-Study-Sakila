3c9aae23a30b5a07ceeae60703b51215fd23e5e1
Vanessa Cunha Rangel Ramos <163733235+vanessacrramos@users.noreply.github.com>
2024-11-14 20:45:06 +0100

    Update exercise_2.sql

--- sql_scripts/exercise_2.sql
+++ sql_scripts/exercise_2.sql
@@ -1,3 +1,4 @@
+# Identify the top 5 customers by total rental spend, including their name, email, and total amount spent.
 SELECT c.first_name, c.last_name, c.email, SUM(p.amount) AS total_amount
 FROM customer c 
 INNER JOIN payment p ON p.customer_id = c.customer_id 
