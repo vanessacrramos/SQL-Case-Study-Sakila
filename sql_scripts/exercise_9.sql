af7a3d9ccee52f0c406d816b37e970b7eaf5af86
Vanessa Cunha Rangel Ramos <163733235+vanessacrramos@users.noreply.github.com>
2024-11-14 20:54:01 +0100

    Update exercise_9.sql

--- sql_scripts/exercise_9.sql
+++ sql_scripts/exercise_9.sql
@@ -1,7 +1,11 @@
-SELECT CONCAT(c.first_name, ' ', c.last_name) AS full_name, c.customer_id, COUNT(*) AS amount_of_movies
+# Calculate how many customers have more than one unreturned DVD.
+WITH customers_late AS (SELECT CONCAT(c.first_name, ' ', c.last_name) AS full_name, c.customer_id, COUNT(*) AS amount_of_movies
 FROM rental r 
 LEFT JOIN customer c ON r.customer_id = c.customer_id 
 WHERE r.return_date IS NULL
 GROUP BY c.customer_id
 HAVING amount_of_movies > 1
-ORDER BY amount_of_movies DESC;
+ORDER BY amount_of_movies DESC)
+
+SELECT COUNT(*)
+FROM customers_late
