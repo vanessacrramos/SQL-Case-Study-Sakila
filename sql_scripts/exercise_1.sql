985c44b7f04b3c4c5796a749f597df911d9832dc
Vanessa Cunha Rangel Ramos <163733235+vanessacrramos@users.noreply.github.com>
2024-11-14 18:29:35 +0100

    Update exercise_1.sql

--- sql_scripts/exercise_1.sql
+++ sql_scripts/exercise_1.sql
@@ -1,1 +1,6 @@
-test
+# Retrieve the first and last names of actors who starred in the movie *Poseidon Forever*.
+SELECT a.first_name, a.last_name
+FROM film f
+INNER JOIN film_actor fa ON f. film_id = fa.film_id
+INNER JOIN actor a ON fa.actor_id = a.actor_id 
+WHERE f.title = 'POSEIDON FOREVER';
